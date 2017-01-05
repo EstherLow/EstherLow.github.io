5d0ce47a133265bd6086c04d2edcdf815d03c92c
Esther Low <phi21416@gmail.com>
2017-01-05 15:59:55 +0800

    added function to evaluate win

--- main.js
+++ main.js
@@ -27,13 +27,16 @@ var gp16 = new Gamepiece('gp16', 'no', 'no', 'round', 'rabbit', 'img/rabbit_rd_f
 var arrayOfGamePieces = [gp1, gp2, gp3, gp4, gp5, gp6, gp7, gp8, gp9, gp10, gp11, gp12, gp13, gp14, gp15, gp16];
 
 var gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-var placedPieces = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0 , 8:0, 9:0, 10:0, 11:0, 12:0, 13:0, 14:0, 15:0}
+
 
 var selectedPiece = '';
 var selectedCell = 0;
 var activePlayer = 1;
-var isGameOver = 0;
-
+var isGameOver = false;
+var winner = 0;
+var row = [];
+var column = [];
+var diagonal = [];
 
 function loadGamePieces () {
 for (var i = 0; i < arrayOfGamePieces.length; i++) {
@@ -55,6 +58,7 @@ function loadGameBoard() {
 loadGameBoard()
 
 function playTurn () {
+  if (isGameOver === true) { alert("Game Over")} else
   if (activePlayer === 1) {
     $(".instructions").append("<h3>Player 2, please select token for Player 1.</h3>")
   } else
@@ -63,24 +67,51 @@ function playTurn () {
 }
 
 
-function winByRows() {
-if (placedPieces[0] && placedPieces[1] && placedPieces[2] && placedPieces[3] !== 0) {
-  console.log("is not empty");
-  // for (var )
+function compareCells ([c1, c2, c3, c4], y) {
+  if ((typeof c1 === "object") && (typeof c2 === "object") && (typeof c3 === "object") && (typeof c4 === "object")) {
+    if ((typeof c1 === 'object') && (c1[y] === c2[y]) && (c2[y] === c3[y]) && (c3[y] === c4[y])) {
+      winner = activePlayer;
+      isGameOver = true;
+      console.log('Match:' + c1[y] + '. Winner is ' + activePlayer);
+    } else {
+      winner = 0;
+      isGameOver = false;
+      // console.log("they are not the same.");
+    }
   }
- }
+}
 
 
-//if (win by row 1) || (win by row 2) || (win by row 3) \\ (win by row 4) {
-// declare winner
-// declare gameOver
-//}
-//else {
-// gameOver is false
-// winner is false
-//}
 
-//function winByColumns () {}
+
+function mapCells() {
+  selectedCell = parseInt(selectedCell)
+  if (selectedCell <= 3) {
+    row = [gameBoard[0], gameBoard[1], gameBoard[2], gameBoard[3]]
+    column = [gameBoard[selectedCell], gameBoard[(selectedCell + 4)], gameBoard[(selectedCell + 8)], gameBoard[(selectedCell + 12)]]
+    if (selectedCell === 0) { diagonal = [gameBoard[0], gameBoard[5], gameBoard[10], gameBoard[15]]; }
+    if (selectedCell === 3) { diagonal = [gameBoard[3], gameBoard[6], gameBoard[9], gameBoard[12]]}
+  }
+  else if (selectedCell <= 7) {
+    row = [gameBoard[4], gameBoard[5], gameBoard[6], gameBoard[7]];
+    column = [gameBoard[(selectedCell - 4)], gameBoard[selectedCell], gameBoard[(selectedCell + 4)], gameBoard[(selectedCell + 8)]]
+    if (selectedCell === 5) {diagonal = [gameBoard[0], gameBoard[5], gameBoard[10], gameBoard[15]];}
+    if (selectedCell === 6) {diagonal = [gameBoard[3], gameBoard[6], gameBoard[9], gameBoard[12]]}
+  }
+  else if (selectedCell <= 11) {
+      row = [gameBoard[8], gameBoard[9], gameBoard[10], gameBoard[11]];
+      column = [gameBoard[(selectedCell - 8)], gameBoard[(selectedCell - 4)], gameBoard[selectedCell], gameBoard[(selectedCell + 4)] ];
+      if (selectedCell === 10) {  diagonal = [gameBoard[0], gameBoard[5], gameBoard[10], gameBoard[15]];}
+      if (selectedCell === 9) {  diagonal = [gameBoard[3], gameBoard[6], gameBoard[9], gameBoard[12]] }
+  }
+  else if (selectedCell <= 15) {
+      row = [gameBoard[12], gameBoard[13], gameBoard[14], gameBoard[15]];
+      column = [gameBoard[(selectedCell - 12)], gameBoard[(selectedCell - 8)], gameBoard[(selectedCell - 4)], gameBoard[selectedCell]];
+      if (selectedCell === 15) { diagonal = [gameBoard[0], gameBoard[5], gameBoard[10], gameBoard[15]]; }
+      if (selectedCell === 12) {diagonal = [gameBoard[3], gameBoard[6], gameBoard[9], gameBoard[12]]}
+    }
+  }
+
 
 
 $('.game-piece').click(function(){
@@ -96,20 +127,34 @@ $('.game-piece').click(function(){
 
 $('.cell').click(function(){
   selectedCell = $(this).attr('id');
-  console.log(selectedCell);
+
   var placed = $('.selected-piece').contents()
+
   if ($("#"+selectedCell).children().length > 0) {
     $('.overlay-msg').css("visibility", "visible")
   } else {
     placed.detach();
     $('#' + selectedCell).append(placed);
   }
-  placedPieces[selectedCell] = window[selectedPiece];
-  winByRows()
-  console.log(placedPieces);
+  gameBoard[selectedCell] = window[selectedPiece];
+  mapCells();
+  compareCells(row, 'animal');
+  compareCells(column,'animal');
+  compareCells(diagonal, 'animal');
+  compareCells(row, 'shape');
+  compareCells(column,'shape');
+  compareCells(diagonal, 'shape');
+  compareCells(row, 'border');
+  compareCells(column,'border')
+  compareCells(diagonal, 'border')
+  compareCells(row, 'ears');
+  compareCells(column,'ears')
+  compareCells(diagonal, 'ears')
+
   if(activePlayer === 1) { activePlayer = 2 } else { activePlayer = 1}
   console.log('active player ' + activePlayer);
-  playTurn()
+  playTurn();
+
 })
 
 $("#dismissed").click(function(){
